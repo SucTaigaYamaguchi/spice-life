62bea643f204cdb58f4d76b47d85c17c6f2c050d
Taiga <user@Taigas-new-MacBook-Pro.local>
2020-10-09 04:12:30 +0900

    20201009

---
+++ slick/common.js
@@ -0,0 +1,11 @@
+    $('.slider').slick({
+//        centerMode: true,
+        //        autoplay :false,
+        autoplay:true,
+        autoplaySpeed: 5000,
+        dots: true,
+//        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+
+    });
